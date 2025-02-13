51f99fd538756ccdceadb40e437abb38bb5cb119
Nicolo Trevisani <nicolo.trevisani89@gmail.com>
2025-02-13 15:33:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 alias rootpy='python3 -i    $TOOLBOX/scripts/rootpy.py'
-alias  roofi='python3 $TOOLBOX/scripts/roofi.py'
+alias  roofi='python3       $TOOLBOX/scripts/roofi.py'
 alias  fitpy='python3 -i    $TOOLBOX/scripts/fitpy.py'
 alias  dnnpy='python3 -i    $TOOLBOX/scripts/dnnpy.py'
 
-alias mrcrab='python       $TOOLBOX/scripts/mrcrab.py'
-alias yieldTable='python   $TOOLBOX/scripts/yieldTable.py'
+alias mrcrab='python3       $TOOLBOX/scripts/mrcrab.py'
+alias yieldTable='python3   $TOOLBOX/scripts/yieldTable.py'
 
-alias condorSubmit='python $TOOLBOX/scripts/condorSubmit.py'
+alias condorSubmit='python3 $TOOLBOX/scripts/condorSubmit.py'
 
 alias summary='python3 -m correctionlib.cli summary'
 
